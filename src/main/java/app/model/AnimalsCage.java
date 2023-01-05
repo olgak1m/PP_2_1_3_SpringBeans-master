8fa67f17afa2a11c741c01cabaaf9bc04cdc823f
olgak <olga.kim0602@gmail.com>
2023-01-05 12:47:34 +0900

    Initial commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,36 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    @Autowired
+    private Timer timer;
+
+    @Bean("dog")
+    public Dog getDog() {
+        return new Dog();
+    }
+
+    @Bean
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+
+    }
+}
